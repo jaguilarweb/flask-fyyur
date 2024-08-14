d2fbd75613ce52f11dec27ae1231cc39177d5960
Jenny Aguilar <contact@jaguilarweb.com>
2024-08-14 20:56:18 +0200

    feat: Add display all artist function

--- src/app.py
+++ src/app.py
@@ -408,17 +408,32 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-  return render_template('pages/artists.html', artists=data)
+  error = False
+  try:
+    # Artist by name
+    data=db.session.query(Artist).all()
+  except():
+    error = True
+    db.session.rollback()
+  finally:
+    db.session.close()
+  if error:
+      flash('An error occurred. Artists could not be listed.')
+      print(sys.exc_info())
+  else:
+      flash('Artists has been successfully listed!')
+      '''data=[{
+          "id": 4,
+          "name": "Guns N Petals",
+        }, {
+          "id": 5,
+          "name": "Matt Quevedo",
+        }, {
+          "id": 6,
+          "name": "The Wild Sax Band",
+        }]'''
+
+      return render_template('pages/artists.html', artists=data)
 
 
 @app.route('/artists/search', methods=['POST'])
